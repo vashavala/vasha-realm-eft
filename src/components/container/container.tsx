89ee8bd6c4c7e9f8b4ca85d20446d918e6d26ac3
vasha <vasha.dev@outlook.com>
2025-03-03 16:52:52 +0800

    feature: nextjs init & basic structure

---
+++ src/components/container/container.tsx
@@ -0,0 +1,9 @@
+const Container = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi minima omnis saepe voluptatum veniam tenetur consequuntur architecto sequi ad fuga earum reiciendis ea consectetur, cumque quaerat odio reprehenderit distinctio fugiat!
+    </div>
+  )
+}
+
+export default Container
